23b23df2c8278584ed30e5755ddedac355e4779c
Steven D Vachon <contact@svachon.com>
2017-08-07 15:47:29 -0400

    Build JSON with objects instead of strings

--- app/routes/charts.js
+++ app/routes/charts.js
@@ -9,21 +9,22 @@ const findAll = (req, res) => {
 };
 
 const findById = (req, res) => {
-  let str = '';
-  switch (req.params.id) {
+  const {id} = req.params;
+  let json;
+  switch (id) {
     case '1':
-      str = `{"id":"${req.params.id}", "name":"mysql", "description":"MySQL Load"}`;
+      json = {id, name:'mysql', description:'MySQL Load'};
       break;
     case '2':
-      str = `{"id":"${req.params.id}", "name":"cpu", "description":"CPU system"}`;
+      json = {id, name:'cpu', description:'CPU system'};
       break;
     case '3':
-      str = `{"id":"${req.params.id}", "name":"disk-sda01", "description":"Disk Writes on /dev/sda01"}`;
+      json = {id, name:'disk-sda01', description:'Disk Writes on /dev/sda01'};
       break;
     default:
-      str = `{"id":"${req.params.id}", "name":null, "description":null}`;
+      json = {id, name:null, description:null};
   }
-  res.send(JSON.parse(str));
+  res.send(json);
 };
 
 const getGraphsById = (req, res) => {
@@ -37,8 +38,16 @@ const getGraphsById = (req, res) => {
   if (mm < 10) {
     mm = '0' + mm;
   }
-  const str = `[{"date":"${yyyy}-${mm}-01", "s1":31, "s2":263, "s3":159, "s4":22, "s5":270},{"date":"${yyyy}-${mm}-02", "s1":14, "s2":260, "s3":158, "s4":12, "s5":276},{"date":"${yyyy}-${mm}-03", "s1":24, "s2":265, "s3":164, "s4":19, "s5":269},{"date":"${yyyy}-${mm}-04", "s1":23, "s2":269, "s3":168, "s4":19, "s5":276},{"date":"${yyyy}-${mm}-05", "s1":25, "s2":272, "s3":168, "s4":17, "s5":279},{"date":"${yyyy}-${mm}-06", "s1":5, "s2":259, "s3":160, "s4":3, "s5":283},{"date":"${yyyy}-${mm}-07", "s1":5, "s2":257, "s3":159, "s4":4, "s5":288}]`;
-  res.send(JSON.parse(str));
+  const json = [
+    {date:`${yyyy}-${mm}-01`, s1:31, s2:263, s3:159, s4:22, s5:270},
+    {date:`${yyyy}-${mm}-02`, s1:14, s2:260, s3:158, s4:12, s5:276},
+    {date:`${yyyy}-${mm}-03`, s1:24, s2:265, s3:164, s4:19, s5:269},
+    {date:`${yyyy}-${mm}-04`, s1:23, s2:269, s3:168, s4:19, s5:276},
+    {date:`${yyyy}-${mm}-05`, s1:25, s2:272, s3:168, s4:17, s5:279},
+    {date:`${yyyy}-${mm}-06`, s1:5,  s2:259, s3:160, s4:3,  s5:283},
+    {date:`${yyyy}-${mm}-07`, s1:5,  s2:257, s3:159, s4:4,  s5:288}
+  ];
+  res.send(json);
 };
 
 const getGraphsForDate = (req, res) => {
@@ -53,33 +62,33 @@ const getGraphsForDate = (req, res) => {
     mm = '0' + mm;
   }
   const newDate = `${yyyy}-${mm}-${dd}`;
-  let str = '';
+  let json;
   switch (dd) {
     case '01':
-      str = `{"date":"${newDate}", "s1":31, "s2":263, "s3":159, "s4":22, "s5":270}`;
+      json = {date:newDate, s1:31, s2:263, s3:159, s4:22, s5:270};
       break;
     case '02':
-      str = `{"date":"${newDate}", "s1":14, "s2":260, "s3":158, "s4":12, "s5":276}`;
+      json = {date:newDate, s1:14, s2:260, s3:158, s4:12, s5:276};
       break;
     case '03':
-      str = `{"date":"${newDate}", "s1":24, "s2":265, "s3":164, "s4":19, "s5":269}`;
+      json = {date:newDate, s1:24, s2:265, s3:164, s4:19, s5:269};
       break;
     case '04':
-      str = `{"date":"${newDate}", "s1":23, "s2":269, "s3":168, "s4":19, "s5":276}`;
+      json = {date:newDate, s1:23, s2:269, s3:168, s4:19, s5:276};
       break;
     case '05':
-      str = `{"date":"${newDate}", "s1":25, "s2":272, "s3":168, "s4":17, "s5":279}`;
+      json = {date:newDate, s1:25, s2:272, s3:168, s4:17, s5:279};
       break;
     case '06':
-      str = `{"date":"${newDate}", "s1":5, "s2":259, "s3":160, "s4":3, "s5":283}`;
+      json = {date:newDate, s1:5,  s2:259, s3:160, s4:3,  s5:283};
       break;
     case '07':
-      str = `{"date":"${newDate}", "s1":5, "s2":257, "s3":159, "s4":4, "s5":288}`;
+      json = {date:newDate, s1:5,  s2:257, s3:159, s4:4,  s5:288};
       break;
     default:
-      str = `{"date":"${newDate}", "s1":0, "s2":0, "s3":0, "s4":0, "s5":0}`;
+      json = {date:newDate, s1:0,  s2:0,   s3:0,   s4:0,  s5:0};
   }
-  res.send(JSON.parse(str));
+  res.send(json);
 };
 
 module.exports = {findAll, findById, getGraphsById, getGraphsForDate};
